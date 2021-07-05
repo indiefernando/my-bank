195f7cec7f5b9ea3a1499b959f6f40697e17e2fd
indiefernando <indie.fernando@Outlook.com>
2021-07-05 15:01:58 +1000

    gitIgnore.bin

--- src/MyBank/src/com/mybank/account/Main.java
+++ src/MyBank/src/com/mybank/account/Main.java
@@ -6,25 +6,9 @@ import MyBank.src.com.mybank.account.SavingsAccount;
 public class Main {
 
     public static void main(String[] args) {
-        CheckingAccount one = new CheckingAccount(
-                "customer one",
-                "customer one account",
-                100.0,
-                "c1"
-        );
-
-        SavingsAccount two = new SavingsAccount(
-                "saver two",
-                "saver two account",
-                100.0,
-                "s2"
-        );
-
-        try {
-            one.transfer(two, 5.0);
-            two.withdraw(3.0);
-        } catch (IllegalArgumentException | UnsupportedOperationException e) {
-            System.out.println(e);
-        }
+        
+        MusicalChairs.playWithSorting();
+        MusicalChairs.playWithMap();
+    	
     }
 }
